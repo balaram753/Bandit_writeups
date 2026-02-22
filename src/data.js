2c0c54121547bb012cf63b9ff46b2bef38ddc722
balaram753 <2400040178@gmail.com>
2026-02-23 02:36:47 +0530

    -

--- src/data.js
+++ src/data.js
@@ -60,7 +60,7 @@ export const writeups = [
     ,
     {
         "id": 5,
-        "title": "Level 4 --> 5",
+        "title": "Level 4 -> 5",
         "description": "The password for the next level is stored in the only human-readable file in the inhere directory. Tip: if your terminal is messed up, try the “reset” command.",
         "commands": [
             "ls",
